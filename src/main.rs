03b54d59b90b05651b6da8375b482efa76f9212c
Boris-Chengbiao Zhou <bobo1239@web.de>
2023-12-02 16:29:51 +0100

    Optimize day 1

--- src/main.rs
+++ src/main.rs
@@ -48,44 +48,47 @@ fn execute_day<I: ?Sized, J: AsRef<I>, S: Display, T: Display>(
 }
 
 fn day1(input: &str) -> Result<(usize, usize)> {
-    // 0/zero is not relevant but makes arithmetic easier below
-    let patterns = &[
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "zero", "one", "two", "three", "four",
-        "five", "six", "seven", "eight", "nine",
-    ];
     // NOTE: regex doesn't work since it doesn't support overlapping matches (look-around)
+    let patterns = &[
+        "\n", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine",
+    ];
     let ac = AhoCorasick::new(patterns)?;
 
     let mut sum_part1 = 0;
     let mut sum_part2 = 0;
-    // OPT: Probably faster to add "\n" as a AhoCorasick pattern
-    for l in input.lines() {
-        let mut first_part1 = 0;
-        let mut last_part1 = 0;
-        let mut first_part2 = 0;
-        let mut last_part2 = 0;
 
-        for mat in ac.find_overlapping_iter(l) {
-            let (digit, real_digit) = match mat.pattern().as_usize() {
-                d @ 0..=9 => (d, true),
-                d => (d - 10, false),
-            };
+    let mut first_part1 = 0;
+    let mut last_part1 = 0;
+    let mut first_part2 = 0;
+    let mut last_part2 = 0;
 
-            if real_digit {
-                if first_part1 == 0 {
-                    first_part1 = digit;
-                }
-                last_part1 = digit;
+    for mat in ac.find_overlapping_iter(input) {
+        let (digit, real_digit) = match mat.pattern().as_usize() {
+            0 => {
+                sum_part1 += first_part1 * 10 + last_part1;
+                sum_part2 += first_part2 * 10 + last_part2;
+                first_part1 = 0;
+                last_part1 = 0;
+                first_part2 = 0;
+                last_part2 = 0;
+                continue;
             }
+            d @ 1..=9 => (d, true),
+            d => (d - 9, false),
+        };
 
-            if first_part2 == 0 {
-                first_part2 = digit;
+        if real_digit {
+            if first_part1 == 0 {
+                first_part1 = digit;
             }
-            last_part2 = digit;
+            last_part1 = digit;
         }
 
-        sum_part1 += first_part1 * 10 + last_part1;
-        sum_part2 += first_part2 * 10 + last_part2;
+        if first_part2 == 0 {
+            first_part2 = digit;
+        }
+        last_part2 = digit;
     }
 
     Ok((sum_part1, sum_part2))
@@ -124,7 +127,7 @@ mod tests {
             7pqrstsixteen
         "};
         assert_eq!(execute_day_input(day1, example_part2)?.1, 281);
-        assert_eq!(execute_day_input(day1, "twone")?.1, 21);
+        assert_eq!(execute_day_input(day1, "twone\n")?.1, 21);
         assert_eq!(execute_day(1, day1, default_input)?, (54916, 54728));
         Ok(())
     }
